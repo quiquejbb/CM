6473a4b04c7fb57a5296628edf611bd2fedca365
Enrique <quiquejbb@gmail.com>
2025-04-19 18:57:57 +0200

    Update VirtualCameraStream.swift

--- CMExtension/VirtualCameraStream.swift
+++ CMExtension/VirtualCameraStream.swift
@@ -1,17 +1,29 @@
 import Foundation
 import CMIOExtension
+import AVFoundation
 
 class VirtualCameraStream: CMIOExtensionStream {
+    private var timer: Timer?
+
     override var formatDescription: CMIOExtensionStreamFormatDescription {
-        // A fake format description with 1280x720 resolution
-        return CMIOExtensionStreamFormatDescription(videoFormat: .init(width: 1280, height: 720, pixelFormatType: kCVPixelFormatType_32BGRA))
+        return CMIOExtensionStreamFormatDescription(
+            videoFormat: .init(width: 1280, height: 720, pixelFormatType: kCVPixelFormatType_32BGRA)
+        )
     }
 
     override func startStream() throws {
         print("📷 Starting virtual camera stream")
+
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0 / 30.0, repeats: true) { _ in
+            print("📦 Sending fake frame at \(Date())")
+        }
+
+        RunLoop.current.add(timer!, forMode: .common)
     }
 
     override func stopStream() {
         print("📷 Stopping virtual camera stream")
+        timer?.invalidate()
+        timer = nil
     }
 }
